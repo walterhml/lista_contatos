3c8cbd1540dc88e976e181fc6d42416f300b78f1
Walther <wszin06@gmail.com>
2024-05-23 20:45:08 -0300

    ContatoDAO crate contato

--- contatoDAO.php
+++ contatoDAO.php
@@ -3,7 +3,8 @@
 require_once 'Database.php';
 require_once 'Contato.php';
 
-class ContatoDAO {    
+class ContatoDAO
+{
     private $db;
 
     public function __construct()
@@ -12,20 +13,41 @@ class ContatoDAO {
     }
 
     // criar um getall
-    public function getAll() {
+    public function getAll()
+    {
         try {
             $sql = "SELECT * FROM contatos_info";
             $stmt = $this->db->prepare($sql);
             $stmt->execute();
             $contatos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-            return array_map(function($contato){
+            return array_map(function ($contato) {
                 return new Contato($contato['id'], $contato['nome'], $contato['telefone'], $contato['email']);
             }, $contatos);
         } catch (PDOException $e) {
             return [];
         }
     }
-}
 
-?>
+    public function create($contato)
+    {
+        try {
+            $sql = "INSERT INTO contatos_info (nome, telefone, email) 
+            VALUES (:nome, :telefone, :email)";
+
+            $stmt = $this->db->prepare($sql);
+            $nome = $contato->getNome();
+            $telefone = $contato->getTelefone();
+            $email = $contato->getEmail();
+
+            $stmt->bindParam(':nome', $nome);
+            $stmt->bindParam(':telefone', $telefone);
+            $stmt->bindParam(':email', $email);
+            
+            return true;
+            
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+}
