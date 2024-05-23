8eac08a78154ec527207876dc9c0ecaa07948a99
Walther <wszin06@gmail.com>
2024-05-22 22:12:04 -0300

    ;

--- contatoDAO.php
+++ contatoDAO.php
@@ -14,7 +14,14 @@ class ContatoDAO {
     // criar um getall
     public function getAll() {
         try {
+            $sql = "SELECT * FROM contatos_info";
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            $contatos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+            return array_map(function($contato){
+                return new Contato($contato['id'], $contato['nome'], $contato['telefone'], $contato['email']);
+            }, $contatos);
         } catch (PDOException $e) {
             return [];
         }
